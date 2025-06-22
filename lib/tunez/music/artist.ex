7f266ad21120bc21e4ee00c29eafc036b830fd25
sangche <sangche.hwang@gmail.com>
2025-06-22 17:52:36 +0900

    add domain interface functions

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -6,18 +6,21 @@ defmodule Tunez.Music.Artist do
     repo Tunez.Repo
   end
 
+  actions do
+    defaults [:create, :read, :update, :destroy]
+    default_accept [:name, :biography]
+  end
+
   attributes do
     uuid_primary_key :id
+
     attribute :name, :string do
-    allow_nil? false
-    end
-    attribute :biography, :string
-    create_timestamp :inserted_at
-    update_timestamp :updated_at
+      allow_nil? false
     end
 
-    actions do
-      defaults [:create, :read, :update, :destroy]
-      default_accept [:name, :biography]
-       end
+    attribute :biography, :string
+
+    create_timestamp :inserted_at
+    update_timestamp :updated_at
+  end
 end
