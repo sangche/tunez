9412a60d2ceea48a80d3e4e823c459c97a6c937b
sangche <sangche.hwang@gmail.com>
2025-06-22 11:28:30 +0900

    applied 'mix igniter.install ash'

--- mix.exs
+++ mix.exs
@@ -34,6 +34,8 @@ defmodule Tunez.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:sourceror, "~> 1.8", only: [:dev, :test]},
+      {:ash, "~> 3.0"},
       {:phoenix, "~> 1.8.0-rc.3", override: true},
       # {:phoenix, "~> 1.7.21"},
       {:phoenix_ecto, "~> 4.5"},
