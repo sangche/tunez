5bf27b129ef4c84f6135d1eb4b56e039a5c8b4b0
sangche <sangche.hwang@gmail.com>
2025-06-24 14:07:46 +0900

    add relationships between artist and album

--- lib/tunez/music/album.ex
+++ lib/tunez/music/album.ex
@@ -5,4 +5,27 @@ defmodule Tunez.Music.Album do
     table "albums"
     repo Tunez.Repo
   end
+
+  attributes do
+    uuid_primary_key :id
+
+    attribute :name, :string do
+      allow_nil? false
+    end
+
+    attribute :year_released, :integer do
+      allow_nil? false
+    end
+
+    attribute :cover_image_url, :string
+
+    create_timestamp :inserted_at
+    update_timestamp :updated_at
+  end
+
+  relationships do
+    belongs_to :artist, Tunez.Music.Artist do
+      allow_nil? false
+    end
+  end
 end
