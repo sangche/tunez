4a7fcd5eef0142701413816582cd0ad229ba34ca
sangche <sangche.hwang@gmail.com>
2025-06-30 15:11:41 +0900

    album identity. need migration for new index in db

--- lib/tunez/music/album.ex
+++ lib/tunez/music/album.ex
@@ -61,5 +61,11 @@ defmodule Tunez.Music.Album do
     end
   end
 
+  # book page 48. need to ash.codegen migrantion
+  identities do
+    identity :unique_album_names_per_artist, [:name, :artist_id],
+      message: "already exists for this artist"
+  end
+
   def next_year, do: Date.utc_today().year + 1
 end
