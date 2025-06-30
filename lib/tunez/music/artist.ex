44d02a554253d97fef4544c86f1cd5e160f60db1
sangche <sangche.hwang@gmail.com>
2025-06-30 13:14:50 +0900

    soring on albums relationship

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -25,6 +25,9 @@ defmodule Tunez.Music.Artist do
   end
 
   relationships do
-    has_many :albums, Tunez.Music.Album
+    has_many :albums, Tunez.Music.Album do
+      # page 45
+      sort year_released: :desc
+    end
   end
 end
