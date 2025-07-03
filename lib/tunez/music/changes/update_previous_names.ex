4bdf26babc843287169f3274f9ca4f9445f1e03b
sangche <sangche.hwang@gmail.com>
2025-07-03 13:24:42 +0900

    Changes run more often than you might think! -make it happen only before real action!

--- lib/tunez/music/changes/update_previous_names.ex
+++ lib/tunez/music/changes/update_previous_names.ex
@@ -1,20 +1,26 @@
 defmodule Tunez.Music.Changes.UpdatePreviousNames do
   use Ash.Resource.Change
 
+  # should be wrapped in hooks such as Ash.Changeset.before_action or Ash.Changeset.after_action.
+  # See page 56 of PDF book
+
   @impl true
   def change(changeset, _opts, _context) do
-    # The code previously in the body of the anonymous change function
-    IO.inspect(changeset, label: "changeset given inside change")
+    Ash.Changeset.before_action(changeset, fn changeset ->
+      IO.inspect(changeset,
+        label: "changeset given inside change in module gets called only before action"
+      )
 
-    new_name = Ash.Changeset.get_attribute(changeset, :name)
-    previous_name = Ash.Changeset.get_data(changeset, :name)
-    previous_names = Ash.Changeset.get_data(changeset, :previous_names)
+      new_name = Ash.Changeset.get_attribute(changeset, :name)
+      previous_name = Ash.Changeset.get_data(changeset, :name)
+      previous_names = Ash.Changeset.get_data(changeset, :previous_names)
 
-    names =
-      [previous_name | previous_names]
-      |> Enum.uniq()
-      |> Enum.reject(fn name -> name == new_name end)
+      names =
+        [previous_name | previous_names]
+        |> Enum.uniq()
+        |> Enum.reject(fn name -> name == new_name end)
 
-    Ash.Changeset.change_attribute(changeset, :previous_names, names)
+      Ash.Changeset.change_attribute(changeset, :previous_names, names)
+    end)
   end
 end
