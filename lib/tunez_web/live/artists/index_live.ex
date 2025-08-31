49f0c14e5edab14da6ac1f825e3d540f665f45fc
sangche <sangche.hwang@gmail.com>
2025-08-31 16:56:22 +0900

    add additional .artist_card_album_info.

--- lib/tunez_web/live/artists/index_live.ex
+++ lib/tunez_web/live/artists/index_live.ex
@@ -64,19 +64,19 @@ defmodule TunezWeb.Artists.IndexLive do
       <.pagination_links page={@page} query_text={@query_text} sort_by={@sort_by} />
     </Layouts.app>
     <!-- A very basic Hero page, prompting the user to either signup or login -->
-<div class="hero min-h-screen bg-base-200">
-    <div class="hero-content text-center">
+    <div class="hero min-h-screen bg-base-200">
+      <div class="hero-content text-center">
         <div class="max-w-md">
-            <h1 class="text-5xl font-bold">Welcome to BasicPay!</h1>
-            <p class="py-6">The app that lets you pay, <em>basically</em>, anyone.</p>
-            <!-- These SvelteKit preload attributes keep the redirects from firing when the user hovers over the button -->
-            <div data-sveltekit-preload-data="tap" data-sveltekit-preload-code="hover">
-                <a href="/signup" class="btn-primary btn">Signup Now</a>
-                <a href="/login" class="btn-outline btn">Login</a>
-            </div>
+          <h1 class="text-5xl font-bold">Welcome to BasicPay!</h1>
+          <p class="py-6">The app that lets you pay, <em>basically</em>, anyone.</p>
+          <!-- These SvelteKit preload attributes keep the redirects from firing when the user hovers over the button -->
+          <div data-sveltekit-preload-data="tap" data-sveltekit-preload-code="hover">
+            <a href="/signup" class="btn-primary btn">Signup Now</a>
+            <a href="/login" class="btn-outline btn">Login</a>
+          </div>
         </div>
+      </div>
     </div>
-</div>
     """
   end
 
@@ -97,6 +97,7 @@ defmodule TunezWeb.Artists.IndexLive do
         {@artist.name}
       </.link>
     </p>
+    <.artist_card_album_info artist={@artist} />
     """
   end
 
