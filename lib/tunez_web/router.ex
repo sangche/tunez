eb834f52ead59e9a2dd9f4e6bbe27dafb06be0d9
sangche <sangche.hwang@gmail.com>
2025-09-06 17:47:47 +0900

    fix "key :current_user not found in session"

--- lib/tunez_web/router.ex
+++ lib/tunez_web/router.ex
@@ -41,6 +41,13 @@ defmodule TunezWeb.Router do
       #
       # If an authenticated user must *not* be present:
       # on_mount {TunezWeb.LiveUserAuth, :live_no_user}
+
+      live "/", Artists.IndexLive
+      live "/artists/new", Artists.FormLive
+      live "/artists/:id", Artists.ShowLive
+      live "/artists/:id/edit", Artists.FormLive
+      live "/artists/:artist_id/albums/new", Albums.FormLive, :new
+      live "/albums/:id/edit", Albums.FormLive, :edit
     end
   end
 
@@ -68,12 +75,8 @@ defmodule TunezWeb.Router do
   scope "/", TunezWeb do
     pipe_through :browser
 
-    live "/", Artists.IndexLive
-    live "/artists/new", Artists.FormLive
-    live "/artists/:id", Artists.ShowLive
-    live "/artists/:id/edit", Artists.FormLive
-    live "/artists/:artist_id/albums/new", Albums.FormLive, :new
-    live "/albums/:id/edit", Albums.FormLive, :edit
+    # moved up
+
     auth_routes AuthController, Tunez.Accounts.User, path: "/auth"
     sign_out_route AuthController
 
