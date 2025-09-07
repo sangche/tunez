50c9b3cdab12e1af4e5701130675e146437d6277
sangche <sangche.hwang@gmail.com>
2025-09-07 16:35:12 +0900

    add json_api support to Tunez.Accounts.User resource by "mix ash.extend Tunez.Accounts.User json_api"

--- lib/tunez_web/ash_json_api_router.ex
+++ lib/tunez_web/ash_json_api_router.ex
@@ -1,6 +1,6 @@
 defmodule TunezWeb.AshJsonApiRouter do
   use AshJsonApi.Router,
-    domains: [Tunez.Music],
+    domains: [Tunez.Music, Tunez.Accounts],
     open_api: "/open_api",
     open_api_title: "Tunez API Documentation 기본적으로는 Open API Specification",
     open_api_version: to_string(Application.spec(:tunez, :vsn))
