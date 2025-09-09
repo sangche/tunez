dc099c835de648115d8b649e9f5e903cd3ec66f0
sangche <sangche.hwang@gmail.com>
2025-09-09 21:26:40 +0900

    Updating forms to check autority at form build time.

--- lib/tunez_web/live/albums/form_live.ex
+++ lib/tunez_web/live/albums/form_live.ex
@@ -6,7 +6,9 @@ defmodule TunezWeb.Albums.FormLive do
       Tunez.Music.get_album_by_id!(album_id, load: [:artist], actor: socket.assigns.current_user)
 
     # artist = Tunez.Music.get_artist_by_id!(album.artist_id)
-    form = Tunez.Music.form_to_update_album(album)
+    form =
+      Tunez.Music.form_to_update_album(album, actor: socket.assigns.current_user)
+      |> AshPhoenix.Form.ensure_can_submit!()
 
     socket =
       socket
@@ -19,7 +21,10 @@ defmodule TunezWeb.Albums.FormLive do
 
   def mount(%{"artist_id" => artist_id}, _session, socket) do
     artist = Tunez.Music.get_artist_by_id!(artist_id, actor: socket.assigns.current_user)
-    form = Tunez.Music.form_to_create_album(artist.id)
+
+    form =
+      Tunez.Music.form_to_create_album(artist.id, actor: socket.assigns.current_user)
+      |> AshPhoenix.Form.ensure_can_submit!()
 
     socket =
       socket
--- lib/tunez_web/live/artists/form_live.ex
+++ lib/tunez_web/live/artists/form_live.ex
@@ -4,7 +4,9 @@ defmodule TunezWeb.Artists.FormLive do
   def mount(%{"id" => artist_id}, _session, socket) do
     artist = Tunez.Music.get_artist_by_id!(artist_id, actor: socket.assigns.current_user)
     # form = AshPhoenix.Form.for_update(artist, :update)
-    form = Tunez.Music.form_to_update_artist(artist)
+    form =
+      Tunez.Music.form_to_update_artist(artist, actor: socket.assigns.current_user)
+      |> AshPhoenix.Form.ensure_can_submit!()
 
     socket =
       socket
@@ -16,7 +18,9 @@ defmodule TunezWeb.Artists.FormLive do
 
   def mount(_params, _session, socket) do
     # form = AshPhoenix.Form.for_create(Tunez.Music.Artist, :create)
-    form = Tunez.Music.form_to_create_artist()
+    form =
+      Tunez.Music.form_to_create_artist(actor: socket.assigns.current_user)
+      |> AshPhoenix.Form.ensure_can_submit!()
 
     socket =
       socket
