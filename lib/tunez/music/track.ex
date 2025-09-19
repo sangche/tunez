f0af31a24552670a17c026fcec373c00d8614bd6
sangche <sangche.hwang@gmail.com>
2025-09-19 17:14:13 +0900

    calculate and prepare :number

--- lib/tunez/music/track.ex
+++ lib/tunez/music/track.ex
@@ -26,6 +26,10 @@ defmodule Tunez.Music.Track do
     end
   end
 
+  preparations do
+    prepare build(load: [:number])
+  end
+
   attributes do
     uuid_primary_key :id
 
@@ -50,6 +54,10 @@ defmodule Tunez.Music.Track do
     belongs_to :album, Tunez.Music.Album do
       allow_nil? false
     end
+  end
+
+  calculations do
+    calculate :number, :integer, expr(order + 1)
   end
 
   # a track belongs to an album, an album has many tracks
