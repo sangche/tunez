9f494b3eb72ac74fa26a442181933ca902232759
sangche <sangche.hwang@gmail.com>
2025-09-19 12:58:36 +0900

    track list of each album on an artist profile

--- lib/tunez/music/track.ex
+++ lib/tunez/music/track.ex
@@ -55,4 +55,5 @@ defmodule Tunez.Music.Track do
   # a track belongs to an album, an album has many tracks
   # no policies, all actions are via tracks form inside the album context for now
   # so policies are inherited from album. see Page 188
+  # If we create Track form directly, we need policies here, to be safe.
 end
