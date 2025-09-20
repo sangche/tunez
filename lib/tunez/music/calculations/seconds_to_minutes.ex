f6fc535ce2b15f6e50272cec73dc6370628f5b7a
sangche <sangche.hwang@gmail.com>
2025-09-20 17:02:06 +0900

    calculation :duration in DB level

--- lib/tunez/music/calculations/seconds_to_minutes.ex
+++ lib/tunez/music/calculations/seconds_to_minutes.ex
@@ -2,24 +2,28 @@ defmodule Tunez.Music.Calculations.SecondsToMinutes do
   use Ash.Resource.Calculation
 
   # load, describe, expression, besides calculate. Page 196-197
-  @impl true
-  def load(query, opts, context) do
-    IO.inspect(query, label: "SecondsToMinutes.load query")
-    IO.inspect(opts, label: "SecondsToMinutes.load opts")
-    IO.inspect(context, label: "SecondsToMinutes.load context")
-    []
-  end
+  # calculations in Elixir code level, using calculate/3 is useful, but it’s not the optimal
+  # way to do things.
 
-  @impl true
-  def calculate(tracks, _opts, _context) do
-    # Code to calculate duration for each track in the list of tracks
-    Enum.map(tracks, fn %{duration_seconds: duration} ->
-      seconds =
-        rem(duration, 60)
-        |> Integer.to_string()
-        |> String.pad_leading(2, "0")
+  # @impl true
+  # def calculate(tracks, _opts, _context) do
+  #   # Code to calculate duration for each track in the list of tracks
+  #   Enum.map(tracks, fn %{duration_seconds: duration} ->
+  #     seconds =
+  #       rem(duration, 60)
+  #       |> Integer.to_string()
+  #       |> String.pad_leading(2, "0")
 
-      "#{div(duration, 60)}:#{seconds}"
-    end)
+  #     "#{div(duration, 60)}:#{seconds}"
+  #   end)
+  # end
+
+  # It’s better to do calculations in the database layer, using expression.
+  # see Page 198.
+  @impl true
+  def expression(_opts, _context) do
+    expr(
+      fragment("? / 60 || to_char(? * interval '1s', ':SS')", duration_seconds, duration_seconds)
+    )
   end
 end
