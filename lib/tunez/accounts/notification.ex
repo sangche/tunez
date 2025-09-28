257e410337d01b102b4f9074548a1b1641c6586a
sangche <sangche.hwang@gmail.com>
2025-09-28 15:22:06 +0900

    Notification belongs_to User, Album

--- lib/tunez/accounts/notification.ex
+++ lib/tunez/accounts/notification.ex
@@ -4,5 +4,25 @@ defmodule Tunez.Accounts.Notification do
   postgres do
     table "notifications"
     repo Tunez.Repo
+
+    references do
+      reference :user, index?: true, on_delete: :delete
+      reference :album, on_delete: :delete
+    end
+  end
+
+  attributes do
+    uuid_primary_key :id
+    create_timestamp :inserted_at
+  end
+
+  relationships do
+    belongs_to :user, Tunez.Accounts.User do
+      allow_nil? false
+    end
+
+    belongs_to :album, Tunez.Music.Album do
+      allow_nil? false
+    end
   end
 end
