82ecf7d2ff2a333c7fa6b855e33655889e0110ef
sangche <sangche.hwang@gmail.com>
2025-09-30 17:54:13 +0900

    comment out change_old

--- lib/tunez/accounts/changes/send_new_album_notifications.ex
+++ lib/tunez/accounts/changes/send_new_album_notifications.ex
@@ -15,17 +15,17 @@ defmodule Tunez.Accounts.Changes.SendNewAlbumNotifications do
     end)
   end
 
-  def change_old(changeset, _opts, _context) do
-    Ash.Changeset.after_action(changeset, fn _changeset, album ->
-      album = Ash.load!(album, artist: [:follower_relationships])
+  # def change_old(changeset, _opts, _context) do
+  #   Ash.Changeset.after_action(changeset, fn _changeset, album ->
+  #     album = Ash.load!(album, artist: [:follower_relationships])
 
-      album.artist.follower_relationships
-      |> Enum.map(fn %{follower_id: follower_id} ->
-        %{album_id: album.id, user_id: follower_id}
-      end)
-      |> Ash.bulk_create!(Tunez.Accounts.Notification, :create, authorize?: false)
+  #     album.artist.follower_relationships
+  #     |> Enum.map(fn %{follower_id: follower_id} ->
+  #       %{album_id: album.id, user_id: follower_id}
+  #     end)
+  #     |> Ash.bulk_create!(Tunez.Accounts.Notification, :create, authorize?: false)
 
-      {:ok, album}
-    end)
-  end
+  #     {:ok, album}
+  #   end)
+  # end
 end
